c2fe4d513f4966d80533362e352dcdc59c1ddfb2
Joel B. <smol@toast.cyou>
2025-11-05 15:22:34 -0500

    enforce new name

--- flake.nix
+++ flake.nix
@@ -62,7 +62,7 @@
 
         postBuild = ''
             # for desktop files
-            install -Dt $out/share/applications resources/grapevine.desktop
+            install -Dt $out/share/applications resources/com.toasterrepair.grapevine.desktop
 
             install -Dt $out/share/icons resources/icon-grapevine.png
         '';
